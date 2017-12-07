b268123da6a846a42b80e0edd8038f511961a84e
Gerardo Prieto <Gerardo_Prieto@westpac.co.nz>
2017-12-08 09:17:00 +1300

    Deleted old files

--- .history/features/pages/googleResults_20171207183358.js
+++
@@ -1,9 +0,0 @@
-const { By, until, Key } = require("selenium-webdriver");
-
-module.exports = driver => ({
-  go: () => driver.get(GOOGLE_URL),
-  search: query => {
-    driver.findElement(By.name("q")).sendKeys(query);
-    driver.findElement(By.name("q")).sendKeys(Key.ENTER);
-  }
-});
--- .history/features/step_definitions/hooks_20171207183003.js
+++
@@ -1,9 +0,0 @@
-const { defineSupportCode } = require("cucumber");
-
-defineSupportCode(function({ Before, After }) {
-  Before(function() {});
-
-  After(function() {
-    return this.driver.quit();
-  });
-});
